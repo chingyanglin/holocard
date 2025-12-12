ee20f85bd8728e5b67a683335b1e5c979433f073
chingyanglin <realchingyanglin@gmail.com>
2025-12-12 20:44:21 +0800

    Initial Commit

---
+++ holocard/holocardApp.swift
@@ -0,0 +1,17 @@
+//
+//  holocardApp.swift
+//  holocard
+//
+//  Created by ChingyangLin on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct holocardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
